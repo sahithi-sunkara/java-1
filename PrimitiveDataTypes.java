fc1f1af237583fda1652d32babd14fce7addac64
Harshitha-sunkavalli12 <151708123+Harshitha-sunkavalli12@users.noreply.github.com>
2024-09-04 15:24:52 +0530

    Create PrimitiveDataTypes.java

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+class PrimitiveDataTypes
+{
+	static int a;
+	static float b;
+	static char c;
+	static double d;
+	static long e;
+	static short f;		
+	static boolean g;
+	static byte h;
+	static String s;
+	public static void main(String args[])
+	{
+		System.out.println("Default value of int      is   "+a);
+		System.out.println("Default value of float    is   "+b);
+		System.out.println("Default value of char     is   "+c);
+		System.out.println("Default value of double   is   "+d);
+		System.out.println("Default value of long     is   "+e);
+		System.out.println("Default value of short    is   "+f);
+		System.out.println("Default value of boolean  is   "+g);
+		System.out.println("Default value of byte     is   "+h);
+		System.out.println("Default value of String   is   "+s);
+	}	
+}
